a00f2ad3ebf5e196ba01ed16f4a5f1d102be593e
Samyakmanwatkar10 <samyakmanwatkar@gmail.com>
2025-07-28 01:45:45 +0530

    loaded dataset

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Importing the dependencies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn import metrics
+
+# importing the dataset
+house_price_dataset=pd.read_csv('housing.csv')
+print(house_price_dataset)
